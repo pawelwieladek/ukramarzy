d9d5c5aac545e806dc44703a95e0f29bf2362678
Pawel Wieladek <pawel.wieladek@allegrogroup.com>
2016-11-08 22:39:39 +0100

    Content

---
+++ script.js
@@ -0,0 +1,87 @@
+if (typeof Object.assign != 'function') {
+  (function () {
+    Object.assign = function (target) {
+      'use strict';
+      // We must check against these specific cases.
+      if (target === undefined || target === null) {
+        throw new TypeError('Cannot convert undefined or null to object');
+      }
+
+      var output = Object(target);
+      for (var index = 1; index < arguments.length; index++) {
+        var source = arguments[index];
+        if (source !== undefined && source !== null) {
+          for (var nextKey in source) {
+            if (source.hasOwnProperty(nextKey)) {
+              output[nextKey] = source[nextKey];
+            }
+          }
+        }
+      }
+      return output;
+    };
+  })();
+}
+
+$(function() {
+    $('.dropdown-toggle').dropdown();
+});
+
+$('.navbar .navbar-nav li a').click(function() {
+    $.scrollTo($($(this).attr('href')), { duration: 100, offset: 70 });
+});
+
+(function($) {
+    var $pswp = $('.pswp')[0];
+    var image = [];
+
+    $('.picture').each( function() {
+        var $pic     = $(this),
+            getItems = function() {
+                var items = [];
+                $pic.find('a').each(function() {
+                    var $href   = $(this).attr('href'),
+                        $size   = $(this).data('size').split('x'),
+                        $width  = $size[0],
+                        $height = $size[1];
+
+                    var item = {
+                        src : $href,
+                        w   : $width,
+                        h   : $height
+                    }
+
+                    items.push(item);
+                });
+                return items;
+            }
+
+        var items = getItems();
+
+        $.each(items, function(index, value) {
+            image[index]     = new Image();
+            image[index].src = value['src'];
+        });
+
+        $pic.on('click', 'figure', function(event) {
+            event.preventDefault();
+
+            var $index = $(this).index();
+            var options = {
+                index: $index,
+                bgOpacity: 0.9,
+                showHideOpacity: true,
+                shareEl: false,
+                zoomEl: false,
+                maxSpreadZoom: 1,
+                getDoubleTapZoom: function(isMouseClick, item) {
+                    return item.initialZoomLevel;
+                },
+                pinchToClose: false
+            }
+
+            var lightBox = new PhotoSwipe($pswp, PhotoSwipeUI_Default, items, options);
+            lightBox.init();
+        });
+    });
+})(jQuery);
