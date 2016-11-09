981a70e5bcfeeca4fc899d39b065c0c6b71e6408
Pawel Wieladek <pawel.wieladek@allegrogroup.com>
2016-11-09 21:28:41 +0100

    Initial commit

--- script.js
+++ script.js
@@ -1,41 +1,16 @@
-if (typeof Object.assign != 'function') {
-  (function () {
-    Object.assign = function (target) {
-      'use strict';
-      // We must check against these specific cases.
-      if (target === undefined || target === null) {
-        throw new TypeError('Cannot convert undefined or null to object');
-      }
-
-      var output = Object(target);
-      for (var index = 1; index < arguments.length; index++) {
-        var source = arguments[index];
-        if (source !== undefined && source !== null) {
-          for (var nextKey in source) {
-            if (source.hasOwnProperty(nextKey)) {
-              output[nextKey] = source[nextKey];
-            }
-          }
-        }
-      }
-      return output;
-    };
-  })();
-}
-
 $(function() {
     $('.dropdown-toggle').dropdown();
 });
 
 $('.navbar .navbar-nav li a').click(function() {
-    $.scrollTo($($(this).attr('href')), { duration: 100, offset: 70 });
+    $(window).scrollTo($($(this).attr('href')), 300, { offset: { left:0, top: -90 }});
 });
 
 (function($) {
     var $pswp = $('.pswp')[0];
     var image = [];
 
-    $('.picture').each( function() {
+    $('.pictures').each( function() {
         var $pic     = $(this),
             getItems = function() {
                 var items = [];
